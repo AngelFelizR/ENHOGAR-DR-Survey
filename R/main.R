a8239b8ff311a15c48b5896558adbec1a74a5b25
”AnFeliz” <anfeliz@phoenixintnl.com>
2023-02-18 10:56:02 -0400

    estudiando personas sordas

--- R/main.R
+++ R/main.R
@@ -1,8 +1,12 @@
 
+RenderReport <- TRUE
+
 # Libraries ----
 
 library(data.table)
 library(fst)
+library(lubridate)
+library(stringr)
 
 MonthToNumber <- 1:12
 
@@ -20,23 +24,8 @@ dificultad_case <- function(x){
         "02 - Cierta dificultad",
         x == "?Dir?a que tiene mucha dificultad?",
         "03 - Mucha dificultad",
-        x == "?Dir?a que tiene mucha dificultad?",
+        x == "?Dir?a que le resulta imposible?",
         "04 - Imposible")
-  
-}
-
-
-render_report<- function(input,
-                         output,
-                         ...){
-  
-  rmarkdown::render(input = file.path("R",input), 
-                    output_file =  output,
-                    ...)
-  file.copy(file.path("R",output), 
-            file.path("output",output),
-            overwrite = TRUE)
-  file.remove(file.path("R",output))
   
 }
 
@@ -45,7 +34,6 @@ render_report<- function(input,
 Personas2021 <-
   read_fst("data/ENHOGAR_BD_2021_PERSONAS.fst",
            as.data.table = TRUE)
-
 
 # Cleaning data ----
 
@@ -79,6 +67,7 @@ for(variable in list.files("data/labels/", pattern = "\\.text$") ){
   
 }
 
+
 ## Changing columns from characters to values ----
 
 
@@ -93,7 +82,7 @@ Personas2021[, (DificultadCols) := lapply(.SD, dificultad_case),
 Personas2021[hay_otra_persona == "S?", hay_otra_persona := "Si"]
 
 
-Personas2021[sexo == "Var?n", hay_otra_persona := "Varón"]
+Personas2021[sexo == "Var?n", sexo := "Varón"]
 
 
 Personas2021[tiene_acta_de_nacimiento == "S? tiene", 
@@ -105,8 +94,13 @@ Personas2021[estado_civil == "Separado(a) de uni?n libre?",
 
 
 Personas2021[estado_civil %like% "\\?$", 
-             estado_civil := sub(pattern = "\\?$", replacement = "", x = estado_civil)]
+             estado_civil := sub(pattern = "\\?$", 
+                                 replacement = "", 
+                                 x = estado_civil)]
 Personas2021[estado_civil == "9", estado_civil := NA_character_]
+
+Personas2021[tipo_empleo == "99", tipo_empleo := NA_character_]
+Personas2021[sabe_leer == "9", sabe_leer := NA_character_]
 
 
 Personas2021[`nun_covid-19_vacunas` == "3 o m?s Dosis", 
@@ -125,39 +119,75 @@ Personas2021[,`:=`(horas_trabajadas_por_semana = as.integer(horas_trabajadas_por
                    nun_vivienda_muestra_registro = as.integer(nun_vivienda_muestra_registro),
                    entrevista_day = NULL,
                    entrevista_month = NULL,
-                   entrevista_year = NULL,
-                   entrevista_date = stringr::str_c(entrevista_year,
-                                                    MonthToNumber[entrevista_month], 
-                                                    entrevista_day,
-                                                    sep = "-") |> as.Date(),
+                   entrevista_year = as.integer(entrevista_year),
+                   entrevista_date = str_c(entrevista_year,
+                                           MonthToNumber[entrevista_month], 
+                                           entrevista_day,
+                                           sep = "-") |> as_date(),
                    nacimiento_day = NULL,
                    nacimiento_month = NULL,
-                   nacimiento_year = NULL,
-                   nacimiento_date = stringr::str_c(nacimiento_year,
-                                                    MonthToNumber[nacimiento_month], 
-                                                    nacimiento_day,
-                                                    sep = "-") |> as.Date(),
+                   nacimiento_year = as.integer(nacimiento_year),
+                   nacimiento_date = str_c(nacimiento_year,
+                                           MonthToNumber[nacimiento_month], 
+                                           nacimiento_day,
+                                           sep = "-") |> as_date(),
                    asiste_a_escuela = fcase(asiste_a_escuela == "No asiste, pero asisti?",
                                             "Asistió",
                                             asiste_a_escuela == "S? asiste",
                                             "Asiste",
                                             asiste_a_escuela == "Nunca asisti?",
-                                            "Nunca asistió"),
+                                            "Nunca asistió") |>
+                                      factor(levels = c("Nunca asistió",
+                                                        "Asiste",
+                                                        "Asistió")),
                    nivel_educativo = fcase(nivel_educativo == "9",
                                            NA_character_,
                                            nivel_educativo == "Primaria o B?sico",
                                            "Primaria o Básico",
                                            nivel_educativo == "Postgrado, Maestr?a o Doctorado",
                                            "Postgrado, Maestría o Doctorado",
-                                           rep(TRUE,.N), nivel_educativo))][]
+                                           rep(TRUE,.N), nivel_educativo) |>
+                                     factor(levels = c("No sabe",
+                                                       "Preescolar o Inicial",
+                                                       "Primaria o Básico",
+                                                       "Secundario o Medio",
+                                                       "Universitario o Superior",
+                                                       "Postgrado, Maestría o Doctorado")),
+                   `grupo_socio-economico` = 
+                     factor(`grupo_socio-economico`,
+                            levels = c("Muy bajo",
+                                       "Bajo",
+                                       "Medio bajo",
+                                       "Medio y Medio alto",
+                                       "Alto")))]
 
 
+# Adding variables ----
+
+Personas2021[,`:=`(id = .I,
+                   edad_entrevista = 
+                      interval(nacimiento_date, entrevista_date) |> 
+                      time_length(unit = "year"),
+                   `Sordera` = dificultad_para_oir == "04 - Imposible")]
+
+
+# Filtering data to study ----
+
+PersonasSolderaConfirm <-
+  Personas2021[!is.na(`Sordera`) &
+                 !is.na(sabe_leer)]
 
 # Creating final report ----
 
-if(!any(dir() == "output")) dir.create("output")
+if(RenderReport){
+  
+  if(!any(dir() == "output")) dir.create("output")
+  
+  rmarkdown::render(input = "R/final-report.Rmd",
+                    output_file = "Final Presentation.html",
+                    output_dir = "output",
+                    envir = .GlobalEnv)
+  
+}
 
-# render_report(input = "final-report.Rmd",
-#               output = "Final Presentation.html",
-#               envir = list2env(list("Personas2021" = Personas2021),  
-#                                parent.frame()))
+
